557d2c36a2f73325369db1f3c46a4349646004cd
AliOding12 <abbasali1214313@gmail.com>
2024-02-28 13:00:00 +0500

    Add Sobel and Laplacian edge detection filters

---
+++ filters/laplacian_filter.py
@@ -0,0 +1,12 @@
+from image_lib.core.utils import convolution
+
+def laplacian_filter(pixels):
+    """
+    Applies Laplacian filter for edge detection.
+    """
+    kernel = [[0, -1, 0],
+              [-1, 4, -1],
+              [0, -1, 0]]
+    
+    return convolution(pixels, kernel)
+# Add Sobel and Laplacian edge detection filters
---
+++ filters/sobel_filter.py
@@ -0,0 +1,29 @@
+from image_lib.core.utils import convolution
+import math
+
+def sobel_filter(pixels):
+    """
+    Applies Sobel edge detection and returns gradient magnitude.
+    """
+    Gx = [[-1, 0, 1],
+          [-2, 0, 2],
+          [-1, 0, 1]]
+
+    Gy = [[-1, -2, -1],
+          [ 0,  0,  0],
+          [ 1,  2,  1]]
+    
+    grad_x = convolution(pixels, Gx)
+    grad_y = convolution(pixels, Gy)
+
+    height = len(pixels)
+    width = len(pixels[0])
+    output = [[0] * width for _ in range(height)]
+
+    for y in range(height):
+        for x in range(width):
+            mag = int(math.sqrt(grad_x[y][x]**2 + grad_y[y][x]**2))
+            output[y][x] = min(255, mag)
+    
+    return output
+# Add Sobel and Laplacian edge detection filters
