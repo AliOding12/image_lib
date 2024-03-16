76cb2fe2b1946344b898ff148f5c3ed5bf6d77c3
AliOding12 <abbasali1214313@gmail.com>
2024-03-16 09:00:00 +0500

    Start tests module with IO tests

---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+from . import test_morphology
+from . import test_transform
+# Start tests module with IO tests
---
+++ tests/test_io.py
@@ -0,0 +1,15 @@
+# tests/test_io.py
+import os
+from image_lib.core.io import write_pgm, read_pgm
+
+def test_pgm_read_write():
+    img = [
+        [0, 128, 255],
+        [255, 128, 0],
+        [50, 100, 150]
+    ]
+    write_pgm("test.pgm", img)
+    read_img = read_pgm("test.pgm")
+    assert img == read_img, "PGM read/write mismatch"
+    os.remove("test.pgm")
+# Start tests module with IO tests
